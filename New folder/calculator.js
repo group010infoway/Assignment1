23774e3f676b769b8a9cc1623d7ba5c8cc843db8
Gourab Karmakar <108367199+Gourab369@users.noreply.github.com>
2022-11-13 00:18:05 +0530

    Update calculator.js

--- New folder/calculator.js
+++ New folder/calculator.js
@@ -1,13 +1,13 @@
-var add=(a,b)=>{
+var add=(a=0,b=0)=>{
     return parseInt(a)+parseInt(b);
 }
-var sub=(a,b)=>{
+var sub=(a=0,b=0)=>{
     return parseInt(a)-parseInt(b);
 }
-var div=(a,b)=>{
+var div=(a=0,b=0)=>{
     return parseInt(a)/parseInt(b);
 }
-var multi=(a,b)=>{
+var multi=(a=0,b=0)=>{
     return parseInt(a)*parseInt(b);
 }
 module.exports={add,sub,div,multi};
